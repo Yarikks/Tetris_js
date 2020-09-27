807ee741950c542e6a06974a04a59940b0c431d5
Yarikks <jasper18poil@gmail.com>
2020-09-28 01:21:28 +0300

    Added comment

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ import Game from './src/game.js';
 
 const game = new Game();
 
+// Adding object "game" to global object window for getting access to game object. Because constants don't fall into global namespace.
+window.game = game;
+
 console.log(game);

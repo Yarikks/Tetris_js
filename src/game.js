26006b77e44eef02e1c6710be5e3a9ee27542384
Yarikks <jasper18poil@gmail.com>
2020-09-28 01:22:12 +0300

    Added methods to Game class.

--- src/game.js
+++ src/game.js
@@ -33,4 +33,62 @@ export default class Game {
             [0, 0, 0]
         ]
     }
+
+    movePieceLeft() {
+        this.activePiece.x -= 1;
+
+        if (this.hasCollision()) {
+            this.activePiece.x += 1;
+        }
+    }
+
+    movePieceRight() {
+        this.activePiece.x += 1;
+
+        if (this.hasCollision()) {
+            this.activePiece.x -= 1;
+        }
+    }
+
+    movePieceDown() {
+        this.activePiece.y += 1;
+
+        if (this.hasCollision()) {
+            this.activePiece.y -= 1;
+            this.lockPiece();
+        }
+    }
+
+    hasCollision() {
+        const { y: pieceY, x: pieceX, blocks } = this.activePiece;
+
+        for (let y = 0; y < blocks.length; y++) {
+            for (let x = 0; x < blocks[y].length; x++) {
+                // first: checking for ones in block
+                // second: checking for playfield bounds
+                // third: checking for ones in playfield
+                if (blocks[y][x] === 1 &&
+                    ((this.playfield[pieceY + y] === undefined || this.playfield[pieceY + y][pieceX + x] === undefined) ||
+                    this.playfield[pieceY + y][pieceX + x] === 1)
+                ) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    lockPiece() {
+        const { y: pieceY, x: pieceX, blocks } = this.activePiece;
+
+        for (let y = 0; y < blocks.length; y++) {
+            for (let x = 0; x < blocks[y].length; x++) {
+                // for ignoring extra zeros in blocks
+                if (blocks[y][x] === 1) {
+                    this.playfield[pieceY + y][pieceX + x] = blocks[y][x];
+                }
+            }
+        }
+    }
 }

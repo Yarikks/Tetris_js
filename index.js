1897516ef008285ad2c4268483d5ac1201a94b7b
Yarikks <jasper18poil@gmail.com>
2020-09-28 14:50:13 +0300

    Added View and keydown EventListener.

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 import Game from './src/game.js';
+import View from './src/view.js';
+
+const root = document.querySelector('#root');
 
 const game = new Game();
+const view = new View(root, 320, 640, 20, 10);
 
 // Adding object "game" to global object window for getting access to game object. Because constants don't fall into global namespace.
 window.game = game;
+window.view = view;
 
-console.log(game);
+document.addEventListener('keydown', event => {
+    switch(event.keyCode){
+        case 37: // left arrow
+            game.movePieceLeft();
+            view.render(game.getState());
+        break;
+        case 38: // up arrow
+            game.rotatePiece();
+            view.render(game.getState())
+        break;
+        case 39: // right arrow
+            game.movePieceRight();
+            view.render(game.getState())
+        break;
+        case 40: // right arrow
+            game.movePieceDown();
+            view.render(game.getState())
+        break;
+    }
+});

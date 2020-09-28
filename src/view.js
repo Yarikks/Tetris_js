22508a37042a6ca78f8b6124fb5ca30c35e680c3
Yarikks <jasper18poil@gmail.com>
2020-09-28 16:55:42 +0300

    Added colors of pieces.

--- src/view.js
+++ src/view.js
@@ -1,4 +1,14 @@
 export default class View {
+    static colors = {
+        '1': 'cyan',
+        '2': 'blue',
+        '3': 'orange',
+        '4': 'yellow',
+        '5': 'green',
+        '6': 'purple',
+        '7': 'red'
+    };
+
     constructor(element, width, height, rows, columns) {
         this.element = element;
         this.width = width;
@@ -34,7 +44,7 @@ export default class View {
                 
                 // block can be 1 or 0 that is equal to true or false
                 if(block){
-                    this.renderBlock(x * this.blockWidth, y * this.blockHeight, this.blockWidth, this.blockHeight, 'red')
+                    this.renderBlock(x * this.blockWidth, y * this.blockHeight, this.blockWidth, this.blockHeight, View.colors[block]);
                 }
             }
         }

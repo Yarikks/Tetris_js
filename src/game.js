9e19327bb521df1d7277d225a2b702ae334a9a57
Yarikks <jasper18poil@gmail.com>
2020-09-28 14:00:12 +0300

    Added rotation method.

--- src/game.js
+++ src/game.js
@@ -59,6 +59,32 @@ export default class Game {
         }
     }
 
+    rotatePiece() {
+        const blocks = this.activePiece.blocks;
+        const length = blocks.length;
+
+        const temp = [];
+        for (let i = 0; i < length; i++) {
+            temp[i] = new Array(length).fill(0);
+        }
+
+        // rotate array
+        // example: 0 1 0       0 1 0
+        //          1 1 1   ->  0 1 1
+        //          0 0 0       0 1 0
+        for (let y = 0; y < length; y++) {
+            for (let x = 0; x < length; x++) {
+                temp[x][y] = blocks[length - 1 - y][x];
+            }
+        }
+
+        this.activePiece.blocks = temp;
+
+        if(this.hasCollision()){
+            this.activePiece.blocks = blocks;
+        }
+    }
+
     hasCollision() {
         const { y: pieceY, x: pieceX, blocks } = this.activePiece;
 
@@ -69,7 +95,7 @@ export default class Game {
                 // third: checking for ones in playfield
                 if (blocks[y][x] === 1 &&
                     ((this.playfield[pieceY + y] === undefined || this.playfield[pieceY + y][pieceX + x] === undefined) ||
-                    this.playfield[pieceY + y][pieceX + x] === 1)
+                        this.playfield[pieceY + y][pieceX + x] === 1)
                 ) {
                     return true;
                 }

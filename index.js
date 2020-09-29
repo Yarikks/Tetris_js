7d91d943b9d4f468d039e09382212829e445fd19
Yarikks <jasper18poil@gmail.com>
2020-09-29 13:16:17 +0300

    Change View size.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import View from './src/view.js';
 const root = document.querySelector('#root');
 
 const game = new Game();
-const view = new View(root, 320, 640, 20, 10);
+const view = new View(root, 480, 640, 20, 10);
 
 // Adding object "game" to global object window for getting access to game object. Because constants don't fall into global namespace.
 window.game = game;

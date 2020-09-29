93e6d877d19f4ea863cfd63e75cabd94011f2eb2
Yarikks <jasper18poil@gmail.com>
2020-09-29 23:43:20 +0300

    Refactoring.

--- index.js
+++ index.js
@@ -1,32 +1,14 @@
 import Game from './src/game.js';
 import View from './src/view.js';
+import Controller from './src/controller.js';
 
 const root = document.querySelector('#root');
 
 const game = new Game();
 const view = new View(root, 480, 640, 20, 10);
+const controller = new Controller(game, view);
 
 // Adding object "game" to global object window for getting access to game object. Because constants don't fall into global namespace.
 window.game = game;
 window.view = view;
-
-document.addEventListener('keydown', event => {
-    switch(event.keyCode){
-        case 37: // left arrow
-            game.movePieceLeft();
-            view.render(game.getState());
-        break;
-        case 38: // up arrow
-            game.rotatePiece();
-            view.render(game.getState())
-        break;
-        case 39: // right arrow
-            game.movePieceRight();
-            view.render(game.getState())
-        break;
-        case 40: // right arrow
-            game.movePieceDown();
-            view.render(game.getState())
-        break;
-    }
-});
+window.controller = controller;

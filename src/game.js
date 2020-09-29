1b885e770597256ae5d4704b51b0131b27a1ef0f
Yarikks <jasper18poil@gmail.com>
2020-09-29 23:42:11 +0300

    Added game over checking and rset method.

--- src/game.js
+++ src/game.js
@@ -6,12 +6,9 @@ export default class Game {
         '4': 80
     };
 
-    score = 0;
-    lines = 0;
-    playfield = this.createPlayfield();
-    activePiece = this.createPiece();
-
-    nextPiece = this.createPiece();
+    constructor() {
+        this.reset();
+    }
 
     get level() {
         return Math.floor(this.lines * 0.1);
@@ -43,8 +40,18 @@ export default class Game {
             level: this.level,
             lines: this.lines,
             nextPiece: this.nextPiece,
-            playfield
+            playfield,
+            isGameOver: this.topOut
         };
+    }
+
+    reset() {
+        this.score = 0;
+        this.lines = 0;
+        this.topOut = false;
+        this.playfield = this.createPlayfield();
+        this.activePiece = this.createPiece();
+        this.nextPiece = this.createPiece();
     }
 
     createPlayfield() {
@@ -144,6 +151,8 @@ export default class Game {
     }
 
     movePieceDown() {
+        if(this.topOut) return;
+
         this.activePiece.y += 1;
 
         if (this.hasCollision()) {
@@ -152,6 +161,10 @@ export default class Game {
             const clearedLines = this.clearLines();
             this.updateScore(clearedLines);
             this.updatePieces();
+        }
+
+        if(this.hasCollision()){
+            this.topOut = true;
         }
     }
 
